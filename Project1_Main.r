fc7673763c7effbedc4e1352c6a3522e1ecabd3b
mattlandry <mattlandry2@gmail.com>
2016-01-09 14:27:46 -0600

    Part 1 code added

--- Project1_Main.r
+++ Project1_Main.r
@@ -11,3 +11,21 @@ Master <- read.table("activity.csv",
                      colClasses = c("numeric","Date","numeric")
 )
 df <- Master
+
+
+TotalStepsByDay <- aggregate(df$steps,list(date=df$date),sum,na.rm=TRUE)
+xAxis = seq(from=0,to=25000,by=2000) 
+hist(TotalStepsByDay$x,
+     breaks = xAxis,
+     main="Total Steps per Day Frequency",
+     col="red",
+     xlab="Steps",
+     ylab="Days",
+     xaxt="n")
+axis(side=1,at=xAxis,labels=xAxis)
+
+
+stepMean <- mean(TotalStepsByDay$x,na.rm=T)
+print(paste("PART 1: The Mean number of steps per day is",round(stepMean,1)))
+stepMedian <- median(TotalStepsByDay$x,na.rm=T)
+print(paste("PART 1: The Median number of steps per day is",round(stepMedian,1)))
