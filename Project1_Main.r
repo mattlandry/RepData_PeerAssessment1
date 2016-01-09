3d7b667dc650e43adde42dbbe5e13d00b6192ee8
mattlandry <mattlandry2@gmail.com>
2016-01-09 14:31:18 -0600

    Code updates
    Merging R code from other R files for different parts of this assignment
    written at different times

--- Project1_Main.r
+++ Project1_Main.r
@@ -29,3 +29,29 @@ stepMean <- mean(TotalStepsByDay$x,na.rm=T)
 print(paste("PART 1: The Mean number of steps per day is",round(stepMean,1)))
 stepMedian <- median(TotalStepsByDay$x,na.rm=T)
 print(paste("PART 1: The Median number of steps per day is",round(stepMedian,1)))
+
+
+#Converting time to something usable for this exercise, format adding 0's where needed
+intHours <- df$interval %/% 100
+intHours <- ifelse(intHours < 10,paste("0",intHours,sep=""),intHours)
+intMinutes <- df$interval %% 100
+intMinutes <- ifelse(intMinutes < 10,paste("0",intMinutes,sep=""),intMinutes)
+
+#Combine the standardized time data back into a time data format
+intTime <- paste(intHours,":",intMinutes,sep="")
+intTime <- strptime(intTime,format="%H:%M")
+df <- cbind(df,intTime)
+
+#Start plotting the time
+SPI <- aggregate(df$steps,list(intTime=df$intTime),mean,na.rm=TRUE)
+plot(SPI$intTime,SPI$x,
+     type = "l",
+     main = "Average Steps per Interval",
+     xlab = "Interval",
+     ylab = "Average Steps")
+
+#Which interval has the highest average?
+MaxStepAvg <- max(SPI$x)
+IntervalWithMaxStepAvg <- SPI$intTime[SPI$x == MaxStepAvg]
+print(paste("The highest average is", MaxStepAvg))
+print(paste("at this time", IntervalWithMaxStepAvg))
